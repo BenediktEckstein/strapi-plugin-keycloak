a309ff44012d647ca51b3e9a2b1cabe6a3de8704
BenediktEckstein <98904315+BenediktEckstein@users.noreply.github.com>
2023-06-20 14:59:39 +0200

    Update create-user.js
    Copy Role on create

--- server/utils/create-user.js
+++ server/utils/create-user.js
@@ -10,15 +10,33 @@ module.exports = async (jwt, strapi) => {
     key: "advanced",
   });
 
-  const role = await strapi
-    .query("plugin::users-permissions.role")
+  const permissionsRole = strapi.query("plugin::users-permissions.role")
+
+  const defaultRole = await permissionsRole
     .findOne({ where: { type: settings.default_role } });
+
+  let roleId = defaultRole.id;
+
+  if (strapi.config?.keycloak?.roleMapping) {
+    const allRoles = await permissionsRole.findMany({});
+
+    let jwtMappedRoles = allRoles.filter((r) => jwt.realm_access.roles.includes(r.name));
+    if (strapi.config?.keycloak?.roleMappingPrefix) jwtMappedRoles = jwtMappedRoles.filter((r) => r.name.startsWith(strapi.config?.keycloak?.roleMappingPrefix));
+
+    if (jwtMappedRoles.length > 1) {
+      console.error(
+        "More than one role found in keycloak.roleMapping. Please check your keycloak.roleMappingPrefix setting and user Roles. ROLES:".jwtMappedRoles.map((r) => r.name).join(", ")
+      );
+    } else if (jwtMappedRoles.length == 1) {
+      roleId = jwtMappedRoles[0].id;
+    }
+  }
 
   return await getService("user").add({
     email: jwt.email,
     confirmed: jwt.email_verified,
     blocked: false,
     username: jwt.name,
-    role: role.id,
+    role: roleId,
   });
 };
