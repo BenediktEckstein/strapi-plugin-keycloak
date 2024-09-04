a4b74946d2f3345c157ea350f21ce0492f0b1b18
BenediktEckstein <98904315+BenediktEckstein@users.noreply.github.com>
2024-09-04 09:30:37 +0200

    Update keycloak-role.js (debug)

--- server/utils/keycloak-role.js
+++ server/utils/keycloak-role.js
@@ -3,15 +3,20 @@ const { roleMappingPrefix, roleMapping } = strapi.config.keycloak;
 // Keycloak role mapping
 module.exports = async (jwt, strapi) => {
     const permissionsRole = strapi.query("api::user-rl.user-rl")
-
+    console.log('permissionsRole' , permissionsRole);
     if (roleMapping) {
         const allRoles = await permissionsRole.findMany({});
+        console.log('allRoles' , allRoles);
+        console.log('jwt.azp' , jwt.azp);
         let jwtRoles = jwt.resource_access[jwt.azp]?.roles || []
+        console.log('jwtRoles' , jwtRoles);
+        console.log('jwt.realm_access',jwt.realm_access)
         if (roleMappingPrefix && jwt.realm_access.roles) {
             jwtRoles.push(...jwt.realm_access.roles.filter(r => r.startsWith(roleMappingPrefix)).map(r => r.substring(roleMappingPrefix.length)));
         }
-        let jwtMappedRoles = allRoles.filter(r => jwtRoles.includes(r.name));
 
+        let jwtMappedRoles = allRoles.filter(r => jwtRoles.includes(r.name));
+        console.log('jwtMappedRoles' , jwtMappedRoles);
         return jwtMappedRoles.map((r) => r.id);
     }
 
